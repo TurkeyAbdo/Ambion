c8c484f97da4132fe95556f68919188192834ab1
TurkeyAbdo <abdulrhmanturkey123@gmail.com>
2025-06-29 12:27:47 +0200

    03:trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-const Page = () => {
+// server call
+// import { caller } from "@/trpc/server";
+// const data = await caller.createAI({text:"Abdulrhman SERVER"})  
+{/* {JSON.stringify(data)} */}
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { Client } from "./client";
+
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Abdulrhman PREFETCH"}))
 
   return (  
     <div >
-      hello world
+      <HydrationBoundary>
+        <Suspense fallback={<p>...Loading</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
